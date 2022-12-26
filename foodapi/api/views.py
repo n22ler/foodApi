d0d474d19e62b629687c7505adeb8481dbd46caa
n22ler <n.lerner.v3@gmail.com>
2022-12-26 15:53:58 +0200

    commdeted

--- foodapi/api/views.py
+++ foodapi/api/views.py
@@ -7,28 +7,28 @@ from .serializers import *
 from .models import *
 
 
-class CatApiView(generics.ListCreateAPIView):
+class CatApiView(generics.ListCreateAPIView): #выдаем все категории
     queryset = food_category.objects.all()
     serializer_class = CategorySer
     permission_classes= (IsAuthenticated,)
 
-class SubApiView(generics.ListCreateAPIView):
+class SubApiView(generics.ListCreateAPIView): #выдаем все подкатегории
     queryset = food_subtype.objects.all()
     serializer_class = SubtypeSer
     permission_classes= (IsAuthenticated,)
 
 
-class ProdApiView(generics.ListCreateAPIView):
+class ProdApiView(generics.ListCreateAPIView): #выдаем все продукты
     queryset = food_product.objects.all()
     serializer_class = ProdSer
     permission_classes= (IsAuthenticated,)
 
-class UserApiView(generics.ListCreateAPIView):
+class UserApiView(generics.ListCreateAPIView): # выдаем пользователя
     queryset = users.objects.all()
     serializer_class= UserSer
     def get_queryset(self):
         return users.objects.filter(id = self.kwargs['id'])
 
-class UpdateUserData(generics.UpdateAPIView):
+class UpdateUserData(generics.UpdateAPIView): # обновляем данные в акк пользователя
     queryset = users.objects.all()
     serializer_class = UpdateUserDataSer
